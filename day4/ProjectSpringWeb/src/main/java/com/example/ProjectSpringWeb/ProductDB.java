3617d1825ac3b53c6a55548600f2deab62cacf33
Armaan <mohammadarmaan533@gmail.com>
2023-05-31 17:42:13 +0530

    day 4

--- day4/ProjectSpringWeb/src/main/java/com/example/ProjectSpringWeb/ProductDB.java
+++ day4/ProjectSpringWeb/src/main/java/com/example/ProjectSpringWeb/ProductDB.java
@@ -1,13 +1,13 @@
-package com.example.ProductSpring;
+package com.example.ProjectSpringWeb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ProductDB extends JpaRepository<Product, Integer> {
 
     Product findByName(String name);
+
 
 }
